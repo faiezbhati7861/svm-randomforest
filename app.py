9cee43913cfe45c11092a5ab576e763a97b315ce
MUHAMMED FAIEZ BHATI <78339511+faiezbhati7861@users.noreply.github.com>
2022-08-01 14:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-model = pickle.load(open('svmmodel.pkl', 'rb')) 
+model = pickle.load(open('svm.pkl', 'rb')) 
 model_randomforest = pickle.load(open('randomforest.pkl', 'rb')) 
 dataset= pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
